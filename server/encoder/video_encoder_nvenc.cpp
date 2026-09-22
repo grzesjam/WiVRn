526da3d3afc5a40ae8e1cc297f1fefdeea63e3bd
Grzegorz M <13075554+grzesjam@users.noreply.github.com>
2026-09-22 18:25:03 +0200

    Increase NVENC VBV buffer to 12 frames

--- server/encoder/video_encoder_nvenc.cpp
+++ server/encoder/video_encoder_nvenc.cpp
@@ -137,8 +137,8 @@ NV_ENC_RC_PARAMS video_encoder_nvenc::get_rc_params(uint64_t bitrate, float fram
 	return {
 	        .rateControlMode = NV_ENC_PARAMS_RC_CBR,
 	        .averageBitRate = static_cast<uint32_t>(bitrate),
-            .vbvBufferSize = static_cast<uint32_t>(bitrate / framerate * 8.0f),
-            .vbvInitialDelay = static_cast<uint32_t>(bitrate / framerate * 2.0f),
+            .vbvBufferSize = static_cast<uint32_t>(bitrate / framerate * 12.0f),
+            .vbvInitialDelay = static_cast<uint32_t>(bitrate / framerate * 8.0f),
 	        .enableLookahead = 0,
 	        .lowDelayKeyFrameScale = 1,
 	        .multiPass = NV_ENC_TWO_PASS_QUARTER_RESOLUTION};
